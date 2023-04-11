88428181bda1c143c31ef75776219332df509fb7
Andrea Torelli <gioran326@protonmail.com>
2023-04-11 15:50:46 +0200

    Header e Jumbo Fatti

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -8,34 +8,32 @@ export default {
 </script>
 
 <template>
-   <div class="container d-flex justify-content-between p-3 align-items-center">
+   <div class="container d-flex justify-content-between p-3 align-items-center fixed-top">
       <div>
          <img src="../../public/img/logo-2.png" alt="Logo Softbox">
       </div>
-      <div class="d-flex">
-         <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                  aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-               </button>
-               <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                  <ul class="navbar-nav">
-                     <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-                     <li class="nav-item"><a class="nav-link" href="#">About Us</a></li>
-                     <li class="nav-item"><a class="nav-link" href="#">Feature</a></li>
-                     <li class="nav-item"><a class="nav-link" href="#">Testimonials</a></li>
-                     <li class="nav-item"><a class="nav-link" href="#">Contact US</a></li>
-                  </ul>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+         <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+               aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+               <ul class="navbar-nav">
+                  <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#">About Us</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#">Feature</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#">Testimonials</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#">Contact US</a></li>
+               </ul>
+               <div class="icons px-4 fw-bolder">
+                  <i class="fa-solid fa-magnifying-glass p-3"></i>
+                  <i class="fa-solid fa-cart-shopping p-3"></i>
                </div>
+               <div class="my-btn">Get Started</div>
             </div>
-         </nav>
-         <div class="icons px-5 fw-bolder">
-            <i class="fa-solid fa-magnifying-glass p-3"></i>
-            <i class="fa-solid fa-cart-shopping"></i>
          </div>
-         <div class="my-btn">Get Started</div>
-      </div>
+      </nav>
    </div>
 </template>
 
@@ -49,4 +47,11 @@ img {
 a {
    color: $ottavo-text;
 }
+
+.my-header{
+   position: fixed;
+   top: 0;
+   left: 0;
+}
+
 </style>
--- src/components/PageJumbo.vue
+++ src/components/PageJumbo.vue
@@ -8,9 +8,27 @@ export default {
 </script>
 
 <template>
-   <h1 class="text-center">Sono PageJumbo</h1>
+   <div class="contenitore d-flex justify-content-between p-5">
+      <div class="testo">
+         <h1>The biggest platform for your growth</h1>
+         <p>There are many variations of passages of Lorem Ipsum available, but majority have suffered alteration in some form</p>
+         <div class="my-btn">Read More</div>
+      </div>
+      <img src="/img/27-1.png" alt="Image Jumbo">
+   </div>
+
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/variables.scss' as *;
 
+.contenitore {
+   max-width: 95rem;
+   margin: 6rem auto 0;
+   background-color: $secondo-bg;
+}
+
+.testo{
+   width: 40%;
+}
 </style>
